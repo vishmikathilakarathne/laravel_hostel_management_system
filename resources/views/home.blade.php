e095303771238b041c270b1d0fc083f712b369c2
vishmikathilakarathne <iit18035@std.uwu.ac.lk>
2022-10-26 05:26:43 -0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
     <!-- Content Row -->
     <div class="row">
 
-        <!-- Earnings (Monthly) Card Example -->
+        <!-- All students details -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
